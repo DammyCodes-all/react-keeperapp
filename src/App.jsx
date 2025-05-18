713bc8fbbb38ca46140d0693fb7786403a9f3a3f
DammyCodes-all <harliarmeen@gmail.com>
2025-05-18 23:23:32 +0100

    Prepare for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import Header from '../components/header.jsx'
-import Notes from '../components/note.jsx'
-
+import Header from './components/header.jsx'
+import Notes from './components/note.jsx'
 
 export default function App(){
     return(
